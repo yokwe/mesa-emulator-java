4c3e2b50328abc691d28c17951ce51756ad5229d
hasegawa <hasegawa@ubuntu-2004.lan>
2020-05-30 19:58:45 +0900

    Add more sanity check

--- src/yokwe/majuro/symbol/antlr/model/TypeSubrange.java
+++ src/yokwe/majuro/symbol/antlr/model/TypeSubrange.java
@@ -1,6 +1,13 @@
 package yokwe.majuro.symbol.antlr.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import yokwe.majuro.UnexpectedException;
+
 public class TypeSubrange extends Type {
+	private static final Logger logger = LoggerFactory.getLogger(TypeSubrange.class);
+
 	public final TypeReference baseType;
 	public final long          valueMin;
 	public final long          valueMax;
@@ -34,6 +41,15 @@ public class TypeSubrange extends Type {
 		if (needsFix) {
 			baseType.fix();
 			if (!baseType.needsFix) {
+				switch(baseType.kind) {
+				case SUBRANGE:
+					break;
+				default:
+					logger.error("Unexpected baseType");
+					logger.error("  baseType {}", baseType);
+					throw new UnexpectedException("Unexpected baseType");
+				}
+				
 				size     = baseType.size;
 				needsFix = false;
 			}
