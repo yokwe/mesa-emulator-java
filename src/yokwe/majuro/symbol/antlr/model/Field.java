e96eef64f49d169e23952b919aaf730484ece7d7
hasegawa <hasegawa@ubuntu-2004.lan>
2020-05-31 15:20:38 +0900

    Make RecordFeild as File in separate file.

---
+++ src/yokwe/majuro/symbol/antlr/model/Field.java
@@ -0,0 +1,117 @@
+package yokwe.majuro.symbol.antlr.model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import yokwe.majuro.UnexpectedException;
+
+abstract class Field {
+	private static final Logger logger = LoggerFactory.getLogger(Type.class);
+
+	public enum TargetKind {
+		TYPE, SELECT
+	}
+	public enum FieldKind {
+		FEILD, BIT_FIELD
+	}
+	
+	public final String    name;
+	public final int       offset;
+	
+	public final TargetKind targetKind;
+	public final FieldKind  fieldKind;
+	
+	public boolean needsFix;
+	public int     size;
+	
+	protected Field(String name, int offset, TargetKind targetKind, FieldKind fieldKind) {
+		this.name       = name;
+		this.offset     = offset;
+		this.targetKind = targetKind;
+		this.fieldKind  = fieldKind;
+		
+		this.needsFix  = true;
+		this.size      = Type.UNKNOWN_SIZE;
+	}
+	
+	@Override
+	public String toString() {
+		logger.error("Unexpected");
+		throw new UnexpectedException("Unexpected");
+	}
+	
+	public abstract void fix();
+}
+
+class FieldType extends Field {
+	public final TypeReference type;
+	
+	protected FieldType(String name, int offset, FieldKind fieldKind, String typeName) {
+		super(name, offset, TargetKind.TYPE, fieldKind);
+		
+		this.type = new TypeReference(name + "#fieldType", typeName);
+	}
+	public FieldType(String name, int offset, String typeName) {
+		this(name, offset, FieldKind.FEILD, typeName);
+	}
+
+	@Override
+	public void fix() {
+		if (type.needsFix) {
+			type.fix();
+			if (!type.needsFix) {
+				size = type.size;
+				needsFix = false;
+			}
+		}
+	}
+}
+class BitFieldType extends FieldType {
+	public final int startPos;
+	public final int stopPos;
+	
+	public BitFieldType(String name, int offset, int startPos, int stopPos, String typeName) {
+		super(name, offset, FieldKind.BIT_FIELD, typeName);
+		
+		this.startPos = startPos;
+		this.stopPos  = stopPos;
+	}
+}
+
+
+
+class FieldSelect extends Field {
+	public final Select select;
+	
+	protected FieldSelect(String name, int offset, FieldKind fieldKind, Select select) {
+		super(name, offset, TargetKind.SELECT, fieldKind);
+		
+		this.select = select;
+	}
+	public FieldSelect(String name, int offset, Select select) {
+		this(name, offset, FieldKind.FEILD, select);
+	}
+
+	@Override
+	public void fix() {
+		if (select.needsFix) {
+			select.fix();
+			if (!select.needsFix) {
+				size = select.size;
+				needsFix = false;
+			}
+		}
+	}
+}
+class BitFieldSelect extends FieldSelect {
+	public final int startPos;
+	public final int stopPos;
+	
+	public BitFieldSelect(String name, int offset, int startPos, int stopPos, Select select) {
+		super(name, offset, FieldKind.BIT_FIELD, select);
+		
+		this.startPos = startPos;
+		this.stopPos  = stopPos;
+	}
+}
+
--- src/yokwe/majuro/symbol/antlr/model/TypeRecord.java
+++ src/yokwe/majuro/symbol/antlr/model/TypeRecord.java
@@ -8,98 +8,11 @@ import org.slf4j.LoggerFactory;
 import yokwe.majuro.UnexpectedException;
 
 public class TypeRecord extends Type {
-	private static final Logger logger = LoggerFactory.getLogger(TypeRecord.class);
+	static final Logger logger = LoggerFactory.getLogger(TypeRecord.class);
 	
-	public static class RecordField {
-		public final String  name;
-		public final int     offset;
-		
-		public final boolean isBitField;
-		public final int     startPos;
-		public final int     stopPos;
-		
-		public final Type    type;
-		public final Select  select;
-		
-		public boolean needsFix;
-		public int     size;
-		
-		
-		private RecordField(String name, int offset, int startPos, int stopPos, Type type, Select select, boolean isBitField) {
-			this.name       = name;
-			this.offset     = offset;
-			
-			this.isBitField = isBitField;
-			this.startPos   = startPos;
-			this.stopPos    = stopPos;
-			
-			this.type       = type;
-			this.select     = select;
-			
-			this.needsFix   = true;
-			this.size       = -1;
-			
-			fix();
-		}
-		RecordField(String name, int offset, int startPos, int stopPos, Type type) {
-			this(name, offset, startPos, stopPos, type, null, true);
-		}
-		RecordField(String name, int offset, Type type) {
-			this(name, offset, -1, -1, type, null, false);
-		}
-		
-		RecordField(String name, int offset, int startPos, int stopPos, Select select) {
-			this(name, offset, startPos, stopPos, null, select, true);
-		}
-		RecordField(String name, int offset, Select select) {
-			this(name, offset, -1, -1, null, select, false);
-		}
-		
-		@Override
-		public String toString() {
-			if (type != null) {
-				if (isBitField) {
-					return String.format("{%s %d %d %d %s}", name, offset, startPos, stopPos, type);
-				} else {
-					return String.format("{%s %d %s}", name, offset, type);
-				}
-			}
-			if (select != null) {
-				if (isBitField) {
-					return String.format("{%s %d %d %d %s}", name, offset, startPos, stopPos, select);
-				} else {
-					return String.format("{%s %d %s}", name, offset, select);
-				}
-			}
-			logger.error("Unexpected");
-			throw new UnexpectedException("Unexpected");
-		}
-		
-		void fix() {
-			if (needsFix) {
-				if (type != null) {
-					type.fix();
-					if (!type.needsFix) {
-						size = type.size;
-						needsFix = false;
-					}
-				}
-				if (select != null) {
-					select.fix();
-					if (!select.needsFix) {
-						size = select.size;
-						needsFix = false;
-					}
-				}
-				logger.error("Unexpected");
-				throw new UnexpectedException("Unexpected");
-			}
-		}
-	}
+	public final List<Field> fieldList;
 	
-	public final List<RecordField> fieldList;
-	
-	TypeRecord(String name, List<RecordField> fieldList) {
+	public TypeRecord(String name, List<Field> fieldList) {
 		super(name, Kind.RECORD);
 		
 		this.fieldList = fieldList;
@@ -117,7 +30,7 @@ public class TypeRecord extends Type {
 		if (needsFix) {
 			boolean foundProblem = false;
 			int recordSize = 0;
-			for(RecordField field: fieldList) {
+			for(Field field: fieldList) {
 				if (field.needsFix) field.fix();
 				if (field.needsFix) {
 					foundProblem = true;
