3e9a410f821bd20f29c268dc3fcf39b224a44258
hasegawa <hasegawa@ubuntu-2004.lan>
2020-05-31 15:19:27 +0900

    Add TypeArrayOpen, TypeArrayFull and TypeArraySubrange

--- src/yokwe/majuro/symbol/antlr/model/TypeArray.java
+++ src/yokwe/majuro/symbol/antlr/model/TypeArray.java
@@ -5,129 +5,36 @@ import org.slf4j.LoggerFactory;
 
 import yokwe.majuro.UnexpectedException;
 
-public class TypeArray extends Type {
+public abstract class TypeArray extends Type {
 	private static final Logger logger = LoggerFactory.getLogger(TypeArray.class);
+	
+	public enum RecordKind {
+		OPEN, SUBRANGE, FULL,
+	}
+	
+	public final RecordKind recordKind;
 
-	public final Type    elementType;
-	public final Type    indexType;
-	public final boolean useIndexTypeRange;
-	public final boolean isOpenArray;
+	public final TypeReference elementType;
+	public final TypeReference indexType;
 	
 	public long rangeMin;
 	public long rangeMax;
 	
-	// for open array
-	public TypeArray(String name, String elementName, String indexName, long rangeMinMax) {
-		super(name, Kind.SUBRANGE, 0);
+	protected TypeArray(String name, int size, RecordKind recordKind, String elementName, String indexName, long rangeMin, long rangeMax) {
+		super(name, Kind.SUBRANGE, size);
 		
-		this.elementType       = new TypeReference(name + "#element", elementName);
-		this.indexType         = new TypeReference(name + "#index", indexName);
-		this.useIndexTypeRange = false;
-		this.isOpenArray       = true;
-		this.rangeMin          = rangeMinMax;
-		this.rangeMax          = rangeMinMax;
-		
-		fix();
-	}
-	public TypeArray(String name, String elementName, long rangeMinMax) {
-		this(name, elementName, "CARDINAL", rangeMinMax);
-	}
-
-	// for subrange array
-	public TypeArray(String name, String elementName, String indexName, long rangeMin, long rangeMax) {
-		super(name, Kind.SUBRANGE);
-		
-		this.elementType       = new TypeReference(name + "#element", elementName);
-		this.indexType         = new TypeReference(name + "#index", indexName);
-		this.useIndexTypeRange = false;
-		this.isOpenArray       = false;
-		this.rangeMin          = rangeMin;
-		this.rangeMax          = rangeMax;
-		
-		fix();
-	}
-	public TypeArray(String name, String elementName, long rangeMin, long rangeMax) {
-		this(name, elementName, "CARDINAL", rangeMin, rangeMax);
-	}
-	
-	// for full range array
-	public TypeArray(String name, String indexName, String elementName) {
-		super(name, Kind.SUBRANGE);
-		
-		this.elementType       = new TypeReference(name + "#element", elementName);
-		this.indexType         = new TypeReference(name + "#index", indexName);
-		this.useIndexTypeRange = true;
-		this.isOpenArray       = false;
-		this.rangeMin          = 0;
-		this.rangeMax          = 0;
+		this.recordKind   = recordKind;
+		this.elementType  = new TypeReference(name + "#element", elementName);
+		this.indexType    = new TypeReference(name + "#index", indexName);
+		this.rangeMin     = rangeMin;
+		this.rangeMax     = rangeMax;
 		
 		fix();
 	}
 	
 	@Override
 	public String toString() {
-		if (useIndexTypeRange) {
-			return String.format("{%s %s %d %s %s}", name, kind, size, elementType, indexType);
-		} else {
-			return String.format("{%s %s %d %s %s %d %d}", name, kind, size, elementType, indexType, rangeMin, rangeMax);
-		}
-	}
-	
-	public void checkIndexType() {
-		if (!indexType.needsFix) {
-			switch(indexType.kind) {
-			case ENUM:
-			{
-				TypeEnum typeEnum = (TypeEnum)indexType;
-				
-				if (useIndexTypeRange) {
-					rangeMin = typeEnum.valueMin;
-					rangeMax = typeEnum.valueMax;
-				} else {
-					if (rangeMin < typeEnum.valueMin) {
-						logger.error("Unexpected rangeMin");
-						logger.error("  rangeMin {}", rangeMin);
-						logger.error("  valueMin {}", typeEnum.valueMin);
-						throw new UnexpectedException("Unexpected rangeMin");
-					}
-					if (typeEnum.valueMax < rangeMax) {
-						logger.error("Unexpected rangeMax");
-						logger.error("  rangeMax {}", rangeMax);
-						logger.error("  valueMax {}", typeEnum.valueMax);
-						throw new UnexpectedException("Unexpected rangeMax");
-					}
-				}
-			}
-				break;
-			case SUBRANGE:
-			{
-				TypeSubrange typeSubrange = (TypeSubrange)indexType;
-				
-				if (useIndexTypeRange) {
-					rangeMin = typeSubrange.valueMin;
-					rangeMax = typeSubrange.valueMax;
-				} else {
-					if (rangeMin < typeSubrange.valueMin) {
-						logger.error("Unexpected rangeMin");
-						logger.error("  rangeMin {}", rangeMin);
-						logger.error("  valueMin {}", typeSubrange.valueMin);
-						throw new UnexpectedException("Unexpected rangeMin");
-					}
-					if (typeSubrange.valueMax < rangeMax) {
-						logger.error("Unexpected rangeMax");
-						logger.error("  rangeMax {}", rangeMax);
-						logger.error("  valueMax {}", typeSubrange.valueMax);
-						throw new UnexpectedException("Unexpected rangeMax");
-					}
-				}
-			}
-				break;
-			default:
-				logger.error("Unexpected indexType");
-				logger.error("  indexType {}", indexType);
-				throw new UnexpectedException("Unexpected indexType");
-			}
-		}
+		return String.format("{%s %s %d %s %s %d %d}", name, kind, size, elementType, indexType, rangeMin, rangeMax);
 	}
 
 	@Override
@@ -136,20 +43,86 @@ public class TypeArray extends Type {
 			if (indexType.needsFix)   indexType.fix();
 			if (elementType.needsFix) elementType.fix();
 			
-			if (!indexType.needsFix) checkIndexType();
-			
 			if (!indexType.needsFix && !elementType.needsFix) {
-				long indexLength = rangeMax - rangeMin;
-				if (Integer.MAX_VALUE < size) {
-					// FIXME
-					logger.error("size is too big");
-					logger.info("    rangeMin", rangeMin);
-					logger.info("    rangeMax", rangeMax);
-					throw new UnexpectedException("size is too big");
+				int length;
+				
+				// sanity check
+				switch(indexType.baseType.kind) {
+				case ENUM:
+				{
+					TypeEnum baseType = (TypeEnum)indexType.baseType;
+					baseType.checkValue(rangeMax, rangeMin);
+					length = baseType.length;
 				}
-				this.size = elementType.size * (int)indexLength;
+					break;
+				case SUBRANGE:
+				{
+					TypeSubrange baseType = (TypeSubrange)indexType.baseType;
+					baseType.checkValue(rangeMax, rangeMin);
+					length = baseType.length;
+				}
+					break;
+				default:
+					logger.error("Unexpected indexType");
+					logger.error("  indexType {}", indexType);
+					throw new UnexpectedException("Unexpected indexType");
+				}
+
+				this.size = elementType.size * length;
 				this.needsFix = false;
 			}
 		}
 	}
 }
+
+class TypeArrayOpen extends TypeArray {
+	TypeArrayOpen(String name, String elementName, String indexName, long rangeMinMax) {
+		super(name, 0, RecordKind.OPEN, elementName, indexName, rangeMinMax, rangeMinMax);
+	}
+	@Override
+	public void fix() {
+		needsFix = false;
+	}
+}
+class TypeArrayFull extends TypeArray {
+	private static final Logger logger = LoggerFactory.getLogger(TypeArrayFull.class);
+
+	TypeArrayFull(String name, String elementName, String indexName) {
+		super(name, Type.UNKNOWN_SIZE, RecordKind.FULL, elementName, indexName, 0, 0);
+	}
+	@Override
+	public void fix() {
+		{
+			indexType.fix();
+			if (!indexType.needsFix) {
+				switch(indexType.baseType.kind) {
+				case ENUM:
+				{
+					TypeEnum baseType = (TypeEnum)indexType.baseType;
+					this.rangeMin = baseType.valueMin;
+					this.rangeMax = baseType.valueMax;
+				}
+					break;
+				case SUBRANGE:
+				{
+					TypeSubrange baseType = (TypeSubrange)indexType.baseType;
+					this.rangeMin = baseType.valueMin;
+					this.rangeMax = baseType.valueMax;
+				}
+					break;
+				default:
+					logger.error("Unexpected indexType");
+					logger.error("  indexType {}", indexType);
+					throw new UnexpectedException("Unexpected indexType");
+				}
+			}
+		}
+		super.fix();
+	}
+}
+class TypeArraySubrange extends TypeArray {
+	TypeArraySubrange(String name, String elementName, String indexName, long rangeMin, long rangeMax) {
+		super(name, Type.UNKNOWN_SIZE, RecordKind.SUBRANGE, elementName, indexName, rangeMin, rangeMax);
+	}
+}
+
