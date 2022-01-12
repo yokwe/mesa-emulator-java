fb308a526684b4c96c0b233d2f2136c54c1b14bd
hasegawa <hasegawa@localhost>
2022-01-12 10:57:09 +0900

    Move common constants to Mesa

--- src/main/java/yokwe/majuro/mesa/Mesa.java
+++ src/main/java/yokwe/majuro/mesa/Mesa.java
@@ -1,6 +1,17 @@
 package yokwe.majuro.mesa;
 
 public final class Mesa {
+	public static final int PAGE_BITS = 8;
+	public static final int PAGE_SIZE = 1 << PAGE_BITS;
+	public static final int PAGE_MASK = PAGE_SIZE - 1;
+	
+	public static final int WORD_BITS = 16;
+	public static final int WORD_SIZE = 1 << WORD_BITS;
+	public static final int WORD_MASK = WORD_SIZE - 1;
+	
+	public static final int MAX_REALMEMORY_PAGE_SIZE = /* RealMemoryImplGuam::largestArraySize */ 4086 * WORD_BITS;
+
+
 	public Memory    memory;
 	public PageCache pageCache;
 	
@@ -28,9 +39,9 @@ public final class Mesa {
 		if (Perf.ENABLED) Perf.readDbl++;
 		int p0 = fetch(va);
 		int p1 = p0 + 1;
-		if ((va & Memory.PAGE_MASK) == Memory.PAGE_MASK) p1 = fetch(va + 1);
+		if ((va & PAGE_MASK) == PAGE_MASK) p1 = fetch(va + 1);
 		
-		return (readWord(p1) << Memory.WORD_SIZE) | (readWord(p0) & Memory.WORD_MASK);
+		return (readWord(p1) << WORD_SIZE) | (readWord(p0) & WORD_MASK);
 	}
 
 }
