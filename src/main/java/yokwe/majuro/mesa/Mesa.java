9c3a8126644fb189188901b937ae2355d05e2331
hasegawa <hasegawa@localhost>
2022-01-12 11:54:28 +0900

    Make methods in Mesa static

--- src/main/java/yokwe/majuro/mesa/Mesa.java
+++ src/main/java/yokwe/majuro/mesa/Mesa.java
@@ -1,6 +1,8 @@
 package yokwe.majuro.mesa;
 
 public final class Mesa {
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Mesa.class);
+
 	public static final int PAGE_BITS = 8;
 	public static final int PAGE_SIZE = 1 << PAGE_BITS;
 	public static final int PAGE_MASK = PAGE_SIZE - 1;
@@ -11,31 +13,39 @@ public final class Mesa {
 	
 	public static final int MAX_REALMEMORY_PAGE_SIZE = /* RealMemoryImplGuam::largestArraySize */ 4086 * WORD_BITS;
 
-
-	public Memory    memory;
-	public PageCache pageCache;
+	private static Memory    memory;
+	private static PageCache pageCache;
 	
-	public Mesa(int vmbits, int rmbits, int ioRegionPage) {
+	public static void init(int vmbits, int rmbits, int ioRegionPage) {
 		memory    = new Memory(vmbits, rmbits, ioRegionPage);
 		pageCache = new PageCache(memory);
 	}
 	
-	public int fetch(int va) {
+	public static void pageFault(int va) {
+		// FIXME
+		logger.info("pageFault {}", Integer.toHexString(va).toUpperCase());
+	}
+	public static void writeProtectFault(int va) {
+		// FIXME
+		logger.info("writeProtectFault {}", Integer.toHexString(va).toUpperCase());
+	}
+	
+	public static int fetch(int va) {
 		if (Perf.ENABLED) Perf.fetch++;
 		return pageCache.fetch(va);
 	}
-	public int store(int va) {
+	public static int store(int va) {
 		if (Perf.ENABLED) Perf.store++;
 		return pageCache.store(va);
 	}
 	
-	public int readWord(int va) {
+	public static int readWord(int va) {
 		return memory.readMemory(va);
 	}
-	public void writeWord(int va, int newValue) {
+	public static void writeWord(int va, int newValue) {
 		memory.writeMemory(va, newValue);
 	}
-	public int readDbl(int va) {
+	public static int readDbl(int va) {
 		if (Perf.ENABLED) Perf.readDbl++;
 		int p0 = fetch(va);
 		int p1 = p0 + 1;
