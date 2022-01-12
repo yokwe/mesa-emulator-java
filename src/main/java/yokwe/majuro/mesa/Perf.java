5273ad4e6988df99ec854bf27f9bd90af73661f0
hasegawa <hasegawa@localhost>
2022-01-12 13:25:56 +0900

    Add dump() method

--- src/main/java/yokwe/majuro/mesa/Perf.java
+++ src/main/java/yokwe/majuro/mesa/Perf.java
@@ -1,6 +1,11 @@
 package yokwe.majuro.mesa;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 public final class Perf {
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Perf.class);
+
 	public static final boolean ENABLED = true;
 	
 	public static long fetchMemory = 0;
@@ -13,4 +18,29 @@ public final class Perf {
 	public static long fetch       = 0;
 	public static long store       = 0;
 	public static long readDbl     = 0;
+	
+	public static void dump() {
+		Class<?> clazz = Perf.class;
+		
+		logger.info("Perf dump START");
+		for(Field field: clazz.getDeclaredFields()) {
+			try {
+				// Skip if field is not static field
+				if (!Modifier.isStatic(field.getModifiers())) continue;
+				// Skip if field type is not long
+				if (!field.getType().equals(Long.TYPE)) continue;
+
+				String name  = field.getName();
+				long   value = field.getLong(null);
+				logger.info("{}", String.format("%-24s %10d", name, value));
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				String exceptionName = e.getClass().getSimpleName();
+				logger.error("{} {}", exceptionName, e);
+				logger.error("clazz {}", clazz);
+				logger.error("field {}", field);
+				throw new Error(exceptionName, e);
+			}
+		}
+		logger.info("Perf dump STOP");
+	}
 }
