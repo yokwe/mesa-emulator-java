44e677b578922e0d7a4b5f426ed8111499ed01f4
hasegawa <hasegawa@localhost>
2022-01-20 17:09:17 +0900

    Add more sanity check of TypeRecord

--- src/main/java/yokwe/majuro/symbol/model/TypeRecord.java
+++ src/main/java/yokwe/majuro/symbol/model/TypeRecord.java
@@ -19,6 +19,7 @@ public class TypeRecord extends Type {
 		public final int    startBit;
 		public final int    stopBit;
 		public final Type   type;
+		public final int    bitSize;
 		
 		public Field(String name, String offset, String startBit, String stopBit, Type type) {
 			this.name     = name;
@@ -26,6 +27,12 @@ public class TypeRecord extends Type {
 			this.startBit = Integer.parseInt(startBit);
 			this.stopBit  = Integer.parseInt(stopBit);
 			this.type     = type;
+			
+			if (this.startBit == NO_VALUE) {
+				this.bitSize  = 0;
+			} else {
+				this.bitSize  = this.stopBit - this.startBit + 1;
+			}
 		}
 		public Field(String name, String offset, Type type) {
 			this(name, offset, NO_VALUE_STRING, NO_VALUE_STRING, type);
@@ -37,7 +44,7 @@ public class TypeRecord extends Type {
 		}
 		
 		public boolean hasStartBit() {
-			return startBit == NO_VALUE;
+			return startBit != NO_VALUE;
 		}
 
 	}
@@ -55,15 +62,30 @@ public class TypeRecord extends Type {
 		this.align     = align;
 		this.fieldList = fieldList;
 		
+		{
+			bitSize = 0;
+			for(var e: fieldList) {
+				bitSize += e.bitSize; // use bitSize of field not e.type.bitSize
+			}
+		}
+
 		// sanity check
+
 		{
 			boolean foundProblem = false;
 			
+			if (8 < bitSize && (bitSize % 16) != 0) {
+				foundProblem = true;
+				logger.error("bitSize is not multiple of 16");
+				logger.error("  {}", String.format("%-30s  %4d:%2d", name, bitSize / 16, bitSize % 16));						
+			}
+			
+			// check duplicate filed name
 			{
 				Map<String, Field> map = new TreeMap<>();
 				for(var e: fieldList) {
 					if (map.containsKey(e.name)) {
-						logger.error("field name has duplication");
+						logger.error("dupblicate field name");
 						logger.error("  new  {}", e);
 						logger.error("  old  {}", map.get(e.name));
 						foundProblem = true;
@@ -72,25 +94,65 @@ public class TypeRecord extends Type {
 					}
 				}
 			}
+			
 			{
-				Map<String, Field> map = new TreeMap<>();
+				Map<Integer, Field> map = new TreeMap<>();
+				
+				// check overlap of field bit position
 				for(var e: fieldList) {
-					String key = String.format("%d %d %d", e.offset, e.startBit, e.stopBit);
-					if (map.containsKey(key)) {
-						logger.error("field position has duplication");
-						logger.error("  new  {}", e);
-						logger.error("  old  {}", map.get(key));
-						foundProblem = true;
-					} else {
-						map.put(key, e);
+					if (e.type.bitSize == 0) continue;
+					if (!e.hasStartBit()) continue;
+					
+					int startBit = e.offset * 16 + e.startBit;
+					int stopBit  = e.offset * 16 + e.stopBit;
+					
+					for(int i = startBit; i <= stopBit; i++) {
+						// check overlap
+						if (map.containsKey(i)) {
+							foundProblem = true;
+							Field o = map.get(i);
+							logger.error("field overlap at bit {}", i);
+							logger.error("{}", String.format("  old %-10s  %2d:%2d - %2d", o.name, o.offset, o.startBit, o.stopBit));
+							logger.error("{}", String.format("  new %-10s  %2d:%2d - %2d", e.name, e.offset, e.startBit, e.stopBit));
+						} else {
+							map.put(i, e);
+						}
+					}
+				}
+				
+				// check unused bit position in record
+				{
+					int minBit = 0;
+					int maxBit = 0;
+					for(var e: fieldList) {
+						if (e.type.bitSize == 0) continue;
+						if (!e.hasStartBit()) continue;
+						
+						int start = e.offset * 16 + e.startBit;
+						int stop  = e.offset * 16 + e.startBit;
+						
+						if (start < minBit) minBit = start;
+						if (maxBit < stop)  maxBit = stop;
+					}
+					
+					for(int i = minBit; i <= maxBit; i++) {
+						if (map.containsKey(i)) {
+							// OK
+						} else {
+							// found hole
+							foundProblem = true;
+							int start = i;
+							int stop  = start;
+							for(int j = i; j <= maxBit; j++) {
+								if (map.containsKey(j)) break;
+								stop = j;
+							}
+							logger.error("hole  {}  {}  {}:{} - {}:{}", name, bitSize, start / 16, start % 16, stop / 16, stop % 16);
+						}
 					}
 				}
 			}
-			{
-				// FIXME check hole in record
-				// FIXME check field overlap
-			}
-			
+
 			if (foundProblem) {
 				logger.error("found problem");
 				logger.error("  name {}", name);
@@ -114,14 +176,6 @@ public class TypeRecord extends Type {
 			}
 			if (countNeedsFix == 0) {
 				needsFix = false;
-				
-				{
-					int n = 0;
-					for(var e: fieldList) {
-						n += e.type.bitSize();
-					}
-					bitSize = n;
-				}
 			}
 		}
 	}
