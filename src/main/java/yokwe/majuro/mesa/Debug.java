324e2e841280743571e56b0f0057f04963976eb9
hasegawa <hasegawa@localhost>
2022-01-21 12:33:26 +0900

    Use more generic name.

--- src/main/java/yokwe/majuro/mesa/Debug.java
+++ src/main/java/yokwe/majuro/mesa/Debug.java
@@ -1,7 +1,7 @@
 package yokwe.majuro.mesa;
 
 public final class Debug {
-	public static final boolean	DISABLE_MEMORY_CACHE  = false; 
-	public static final boolean ENABLE_SUBRANGE_CHECK = false;
+	public static final boolean	DISABLE_MEMORY_CACHE = false; 
+	public static final boolean ENABLE_CHECK_VALUE   = false;
 	
 }
