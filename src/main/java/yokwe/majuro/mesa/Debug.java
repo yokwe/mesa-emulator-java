de47541b0aa293d8289a2fb6a5aea976f4a32444
hasegawa <hasegawa@localhost>
2022-01-23 11:58:31 +0900

    Remove DISABLE_MEMORY_CACHE

--- src/main/java/yokwe/majuro/mesa/Debug.java
+++ src/main/java/yokwe/majuro/mesa/Debug.java
@@ -1,7 +1,6 @@
 package yokwe.majuro.mesa;
 
 public final class Debug {
-	public static final boolean	DISABLE_MEMORY_CACHE = false; 
 	public static final boolean ENABLE_CHECK_VALUE   = false;
 	
 }
