b90f078b3f35a7619ad3f34fc5aea20f35c8e764
hasegawa <hasegawa@localhost>
2022-02-15 18:00:33 +0900

    Add more test cases.

---
+++ src/test/java/yokwe/majuro/mesa/BytePairTest.java
@@ -0,0 +1,47 @@
+package yokwe.majuro.mesa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+import yokwe.majuro.type.BytePair;
+
+public class BytePairTest extends Base {
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(BytePairTest.class);
+
+	@Test
+	public void valueA() {
+		logger.info("valueA");
+
+		char value = 0x89AB;
+
+		// prepare
+		// execute
+		BytePair bytePair = BytePair.value(value);
+		
+		// check result
+		assertEquals(value >>> 8, bytePair.left());
+		assertEquals(value & 0xFF, bytePair.right());
+		assertEquals(value, bytePair.value);
+	}
+
+	@Test
+	public void value_B() {
+		logger.info("valueB");
+
+		char left  = 0x89;
+		char right = 0xAB;
+
+		// prepare
+		// execute
+		BytePair bytePair = BytePair.value((char)0);
+		bytePair.left(left);
+		bytePair.right(right);
+		
+		// check result
+		assertEquals(left,  bytePair.left());
+		assertEquals(right, bytePair.right());
+		assertEquals(left << 8 | right, bytePair.value);
+	}
+
+}
---
+++ src/test/java/yokwe/majuro/mesa/LongTest.java
@@ -0,0 +1,66 @@
+package yokwe.majuro.mesa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+import yokwe.majuro.type.Long;
+import yokwe.majuro.type.MemoryAccess;
+
+public class LongTest extends Base {
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(LongTest.class);
+
+	@Test
+	public void valueA() {
+		logger.info("valueA");
+
+		int value = 0x89AB_CDEF;
+
+		// prepare
+		// execute
+		Long object = Long.value(value);
+		
+		// check result
+		assertEquals(value & 0xFFFF, object.low());
+		assertEquals(value >>> 16, object.high());
+	}
+	@Test
+	public void valueB() {
+		logger.info("valueB");
+
+		int  value = 0xabcd1234;
+		char low   = Mesa.lowHalf(value);
+		char high  = Mesa.highHalf(value);
+
+		// prepare
+		// execute
+		Long object = Long.value(0);
+		object.low(low);
+		object.high(high);
+		
+		// check result
+		assertEquals(low,  object.low());
+		assertEquals(high, object.high());
+		assertEquals(value, object.value);
+	}
+	
+	@Test
+	public void readA() {
+		logger.info("valueA");
+
+		int va    = 0x20_1234;
+		int value = 0x89AB_CDEF;
+
+		// prepare
+		Mesa.write32(va, value);
+		
+		// execute
+		Long object = Long.longPointer(va, MemoryAccess.READ);
+		
+		// check result
+		assertEquals(Mesa.lowHalf(value), object.low());
+		assertEquals(Mesa.highHalf(value), object.high());
+	}
+
+
+}
