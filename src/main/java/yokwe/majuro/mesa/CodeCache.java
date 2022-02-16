5e30b9ce4811c33bb2aa7fcece89f358988079a5
hasegawa <hasegawa@localhost>
2022-02-16 11:41:30 +0900

    Modify CodeCache to use Memory.isSamePage()

--- src/main/java/yokwe/majuro/mesa/CodeCache.java
+++ src/main/java/yokwe/majuro/mesa/CodeCache.java
@@ -1,5 +1,77 @@
 package yokwe.majuro.mesa;
 
+public final class CodeCache {
+	private static int cb;
+	private static int pc;
+
+	public static int CB() {
+		return cb;
+	}
+	public static void CB(int newValue) {
+		cb = newValue;
+		invalidate();
+	}
+	
+	public static char PC() {
+		return (char)pc;
+	}
+	public static void PC(int newValue) {
+		pc = newValue & 0xFFFF;
+	}
+	
+	private static int vaPage;
+	private static int raPage;
+	private static int va;
+	private static int ra;
+	
+	private static int vaLast;
+	private static int word;
+	
+	private static void invalidate() {
+		va     = 0;
+		vaLast = ~0; // make (va == vaLast) false
+		vaPage = ~0; // make Memory.isSamePage(vaPage, va) false
+	}
+	
+	public static int getCodeByte() {
+		if (Perf.ENABLED) Perf.codeCacheCodeByte++;
+		va = cb + (pc / 2);
+		if (va == vaLast) {
+			// point to same address again
+			if (Perf.ENABLED) Perf.codeCacheSameLast++;
+		} else {
+			if (Memory.isSamePage(vaPage, va)) {
+				if (Perf.ENABLED) Perf.codeCacheSamePage++;
+				ra = raPage + (va & Constants.PAGE_MASK);
+			} else {
+				if (Perf.ENABLED) Perf.codeCacheNotSamePage++;
+				ra = Memory.instance.fetch(va);
+				vaPage = va & ~Constants.PAGE_MASK;
+				raPage = ra & ~Constants.PAGE_MASK;
+				word = Memory.instance.readReal16(ra);
+			}
+		}
+		vaLast = va;
+		
+		// increment pc
+		pc = (pc + 1) & 0xFFFF;
+		
+		//       even             left           right        
+		return (((pc & 1) == 0) ? (word >>> 8) : word) & 0xFF;
+	}
+	
+	public static int getCodeWord() {
+		if (Perf.ENABLED) Perf.codeCacheCodeWord++;
+		int left  = getCodeByte();
+		int right = getCodeByte();
+		
+		return (left << 8) | right;
+	}
+	
+}
+
+
+/*
 public class CodeCache {
 	private static final int PAGE_SIZE_IN_BYTE = Constants.PAGE_SIZE * 2;
 	private static final int PAGE_MASK_IN_BYTE = PAGE_SIZE_IN_BYTE - 1;
@@ -96,6 +168,5 @@ public class CodeCache {
 			wordOffset = (-startPC) / 2;
 		}
 	}
-
-	
 }
+*/
