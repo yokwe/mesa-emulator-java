5c8482b2fd60e1194835ff152d3fd3de132039dd
hasegawa <hasegawa@localhost>
2022-02-17 10:59:59 +0900

    Add NibblePair related methods.

--- src/main/java/yokwe/majuro/mesa/Types.java
+++ src/main/java/yokwe/majuro/mesa/Types.java
@@ -3,6 +3,9 @@ package yokwe.majuro.mesa;
 import static yokwe.majuro.mesa.Constants.*;
 
 public class Types {
+	//
+	// Long
+	//
 	// returns low order word of double word
 	public static char lowHalf(int value) {
 		return (char)value;
@@ -15,4 +18,16 @@ public class Types {
 		return (high << WORD_BITS) | (low & WORD_MASK);
 	}
 
+	//
+	// NibblePair
+	//
+	public static int nibblePairLeft(int value) {
+		return (value >>> 4) & 0x0F;
+	}
+	public static int nibblePairRight(int value) {
+		return value & 0x0F;
+	}
+	public static int makeNibblePair(int left, int right) {
+		return ((left & 0x0F) << 4) | (right & 0x0F);
+	}
 }
