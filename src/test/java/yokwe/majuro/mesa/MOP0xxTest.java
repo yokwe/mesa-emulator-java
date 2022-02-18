dbdfee319617c09f3a567d51a94aa0798e4b398e
hasegawa <hasegawa@localhost>
2022-02-18 17:17:29 +0900

    Minor modification

--- src/test/java/yokwe/majuro/mesa/MOP0xxTest.java
+++ src/test/java/yokwe/majuro/mesa/MOP0xxTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-
 public class MOP0xxTest extends Base {
 	private static final yokwe.majuro.util.FormatLogger logger = yokwe.majuro.util.FormatLogger.getLogger();
 
