bb5d6626681a718c033ff505a900ae9df5d02c28
hasegawa <hasegawa@localhost>
2022-02-19 13:29:39 +0900

    Annotate mesa type.

--- src/main/java/yokwe/majuro/mesa/Types.java
+++ src/main/java/yokwe/majuro/mesa/Types.java
@@ -1,33 +1,34 @@
 package yokwe.majuro.mesa;
 
-import static yokwe.majuro.mesa.Constants.*;
+import static yokwe.majuro.mesa.Constants.WORD_BITS;
+import static yokwe.majuro.mesa.Constants.WORD_MASK;
 
 public final class Types {
 	//
 	// Long
 	//
 	// returns low order word of double word
-	public static int lowHalf(int value) {
+	public static @Mesa.CARD16 int lowHalf(@Mesa.CARD32 int value) {
 		return value & 0xFFFF;
 	}
 	// returns high order word of double word
-	public static int highHalf(int value) {
+	public static @Mesa.CARD16 int highHalf(@Mesa.CARD32 int value) {
 		return value >>> WORD_BITS;
 	}
-	public static int toCARD32(int high, int low) {
+	public static @Mesa.CARD32 int toCARD32(@Mesa.CARD16 int high, @Mesa.CARD16 int low) {
 		return (high << WORD_BITS) | (low & WORD_MASK);
 	}
 
 	//
 	// toCARD8
 	//
-	public static int toCARD8(int value) {
+	public static @Mesa.CARD8 int toCARD8(int value) {
 		return value & 0xFF;
 	}
 	//
 	// toCARD16
 	//
-	public static int toCARD16(int value) {
+	public static @Mesa.CARD16 int toCARD16(int value) {
 		return value & 0xFFFF;
 	}
 }
