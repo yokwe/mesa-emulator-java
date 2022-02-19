e6d40877342988e7157d2d8d689e53f66dd8baa9
hasegawa <hasegawa@localhost>
2022-02-19 10:48:42 +0900

    Add mesa type annotation.

--- src/main/java/yokwe/majuro/mesa/Mesa.java
+++ src/main/java/yokwe/majuro/mesa/Mesa.java
@@ -1,6 +1,58 @@
 package yokwe.majuro.mesa;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 public final class Mesa {
-//	private static final yokwe.majuro.util.FormatLogger logger = yokwe.majuro.util.FormatLogger.getLogger();
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+	public @interface BOOL {}
 	
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+	public @interface ENUM {}
+	
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+	public @interface PDA_POINTER {}
+	
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+	public @interface SHORT_POINTER {}
+	
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+	public @interface POINTER {}
+	
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+	public @interface REAL_POINTER {}
+	
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+	public @interface CARD8 {}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+	public @interface CARD16 {}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+	public @interface CARD32 {}
+	
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+	public @interface INT8 {}
+	
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+	public @interface INT16 {}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+	public @interface INT32 {}
+
 }
