b8cffbf309d6d7f787a1b65d0ba57a6a92993ab9
hasegawa <hasegawa@localhost>
2022-02-19 23:41:44 +0900

    Add MemoryAcccess access to MemoryBase

--- src/main/java/yokwe/majuro/type/MemoryBase.java
+++ src/main/java/yokwe/majuro/type/MemoryBase.java
@@ -4,8 +4,10 @@ import yokwe.majuro.mesa.Mesa;
 
 public class MemoryBase {
 	public final @Mesa.LONG_POINTER int base;
- 
-    public MemoryBase(@Mesa.LONG_POINTER int base) {
-    	this.base = base;
+	public final MemoryAccess access;
+	
+    public MemoryBase(@Mesa.LONG_POINTER int base, MemoryAccess access) {
+    	this.base   = base;
+    	this.access = access;
     }
 }
