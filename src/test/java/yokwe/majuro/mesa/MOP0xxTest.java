96f242e359f8eee568a451dadea64bb958e3fdcd
hasegawa <hasegawa@localhost>
2022-02-20 23:37:14 +0900

    Temporary commit.

--- src/test/java/yokwe/majuro/mesa/MOP0xxTest.java
+++ src/test/java/yokwe/majuro/mesa/MOP0xxTest.java
@@ -6,18 +6,20 @@ import org.junit.jupiter.api.Test;
 
 import yokwe.majuro.opcode.Interpreter;
 import yokwe.majuro.opcode.Opcode;
+import yokwe.majuro.util.StackUtil;
 
 public class MOP0xxTest extends Base {
 	private static final yokwe.majuro.util.FormatLogger logger = yokwe.majuro.util.FormatLogger.getLogger();
 	
 	@Test
 	public void LL0() {
-		logger.info("LL0");
+		logger.info(StackUtil.getCallerMethodName());
 
 		int n = 0;
+		var opcode = Opcode.LL0;
 		
 		// opcode
-		Memory.writeReal16(ra_PC + 0, Types.toCARD16(Opcode.LL0.code, 0));
+		Memory.writeReal16(ra_PC + 0, Types.toCARD16(opcode.code, 0));
 		// data
 		Memory.writeReal16(ra_LF + n, 0xCAFE);
 		// execute
@@ -34,12 +36,13 @@ public class MOP0xxTest extends Base {
 	
 	@Test
 	public void LL1() {
-		logger.info("LL1");
+		logger.info(StackUtil.getCallerMethodName());
 
 		int n = 1;
+		var opcode = Opcode.LL1;
 		
 		// opcode
-		Memory.writeReal16(ra_PC + 0, Types.toCARD16(Opcode.LL1.code, 0));
+		Memory.writeReal16(ra_PC + 0, Types.toCARD16(opcode.code, 0));
 		// data
 		Memory.writeReal16(ra_LF + n, 0xCAFE);
 		// execute
@@ -56,12 +59,13 @@ public class MOP0xxTest extends Base {
 	
 	@Test
 	public void LL2() {
-		logger.info("LL2");
+		logger.info(StackUtil.getCallerMethodName());
 
 		int n = 2;
+		var opcode = Opcode.LL2;
 		
 		// opcode
-		Memory.writeReal16(ra_PC + 0, Types.toCARD16(Opcode.LL2.code, 0));
+		Memory.writeReal16(ra_PC + 0, Types.toCARD16(opcode.code, 0));
 		// data
 		Memory.writeReal16(ra_LF + n, 0xCAFE);
 		// execute
@@ -78,12 +82,13 @@ public class MOP0xxTest extends Base {
 	
 	@Test
 	public void LL3() {
-		logger.info("LL3");
+		logger.info(StackUtil.getCallerMethodName());
 
 		int n = 3;
+		var opcode = Opcode.LL3;
 		
 		// opcode
-		Memory.writeReal16(ra_PC + 0, Types.toCARD16(Opcode.LL3.code, 0));
+		Memory.writeReal16(ra_PC + 0, Types.toCARD16(opcode.code, 0));
 		// data
 		Memory.writeReal16(ra_LF + n, 0xCAFE);
 		// execute
@@ -100,12 +105,13 @@ public class MOP0xxTest extends Base {
 	
 	@Test
 	public void LL4() {
-		logger.info("LL4");
+		logger.info(StackUtil.getCallerMethodName());
 
 		int n = 4;
+		var opcode = Opcode.LL4;
 		
 		// opcode
-		Memory.writeReal16(ra_PC + 0, Types.toCARD16(Opcode.LL4.code, 0));
+		Memory.writeReal16(ra_PC + 0, Types.toCARD16(opcode.code, 0));
 		// data
 		Memory.writeReal16(ra_LF + n, 0xCAFE);
 		// execute
@@ -120,5 +126,188 @@ public class MOP0xxTest extends Base {
 		// memory
 	}
 
+	@Test
+	public void LL5() {
+		logger.info(StackUtil.getCallerMethodName());
+
+		int n = 5;
+		var opcode = Opcode.LL5;
+		
+		// opcode
+		Memory.writeReal16(ra_PC + 0, Types.toCARD16(opcode.code, 0));
+		// data
+		Memory.writeReal16(ra_LF + n, 0xCAFE);
+		// execute
+		Interpreter.execute();
+		// check result
+		// pc
+		assertEquals(Processor.savedPC + 1, Memory.PC());
+		// sp
+		assertEquals(1, Processor.SP);
+		// stack contents
+		assertEquals(Memory.readReal16(ra_LF + n), Processor.stack[0]);
+		// memory
+	}
+
+	@Test
+	public void LL6() {
+		logger.info(StackUtil.getCallerMethodName());
+
+		int n = 6;
+		var opcode = Opcode.LL6;
+		
+		// opcode
+		Memory.writeReal16(ra_PC + 0, Types.toCARD16(opcode.code, 0));
+		// data
+		Memory.writeReal16(ra_LF + n, 0xCAFE);
+		// execute
+		Interpreter.execute();
+		// check result
+		// pc
+		assertEquals(Processor.savedPC + 1, Memory.PC());
+		// sp
+		assertEquals(1, Processor.SP);
+		// stack contents
+		assertEquals(Memory.readReal16(ra_LF + n), Processor.stack[0]);
+		// memory
+	}
+
+	@Test
+	public void LL7() {
+		logger.info(StackUtil.getCallerMethodName());
+
+		int n = 7;
+		var opcode = Opcode.LL7;
+		
+		// opcode
+		Memory.writeReal16(ra_PC + 0, Types.toCARD16(opcode.code, 0));
+		// data
+		Memory.writeReal16(ra_LF + n, 0xCAFE);
+		// execute
+		Interpreter.execute();
+		// check result
+		// pc
+		assertEquals(Processor.savedPC + 1, Memory.PC());
+		// sp
+		assertEquals(1, Processor.SP);
+		// stack contents
+		assertEquals(Memory.readReal16(ra_LF + n), Processor.stack[0]);
+		// memory
+	}
+
+	@Test
+	public void LL8() {
+		logger.info(StackUtil.getCallerMethodName());
+
+		int n = 8;
+		var opcode = Opcode.LL8;
+		
+		// opcode
+		Memory.writeReal16(ra_PC + 0, Types.toCARD16(opcode.code, 0));
+		// data
+		Memory.writeReal16(ra_LF + n, 0xCAFE);
+		// execute
+		Interpreter.execute();
+		// check result
+		// pc
+		assertEquals(Processor.savedPC + 1, Memory.PC());
+		// sp
+		assertEquals(1, Processor.SP);
+		// stack contents
+		assertEquals(Memory.readReal16(ra_LF + n), Processor.stack[0]);
+		// memory
+	}
+
+	@Test
+	public void LL9() {
+		logger.info(StackUtil.getCallerMethodName());
+
+		int n = 9;
+		var opcode = Opcode.LL9;
+		
+		// opcode
+		Memory.writeReal16(ra_PC + 0, Types.toCARD16(opcode.code, 0));
+		// data
+		Memory.writeReal16(ra_LF + n, 0xCAFE);
+		// execute
+		Interpreter.execute();
+		// check result
+		// pc
+		assertEquals(Processor.savedPC + 1, Memory.PC());
+		// sp
+		assertEquals(1, Processor.SP);
+		// stack contents
+		assertEquals(Memory.readReal16(ra_LF + n), Processor.stack[0]);
+		// memory
+	}
+
+	@Test
+	public void LL10() {
+		logger.info(StackUtil.getCallerMethodName());
+
+		int n = 10;
+		var opcode = Opcode.LL10;
+		
+		// opcode
+		Memory.writeReal16(ra_PC + 0, Types.toCARD16(opcode.code, 0));
+		// data
+		Memory.writeReal16(ra_LF + n, 0xCAFE);
+		// execute
+		Interpreter.execute();
+		// check result
+		// pc
+		assertEquals(Processor.savedPC + 1, Memory.PC());
+		// sp
+		assertEquals(1, Processor.SP);
+		// stack contents
+		assertEquals(Memory.readReal16(ra_LF + n), Processor.stack[0]);
+		// memory
+	}
+
+	@Test
+	public void LL11() {
+		logger.info(StackUtil.getCallerMethodName());
+
+		int n = 11;
+		var opcode = Opcode.LL11;
+		
+		// opcode
+		Memory.writeReal16(ra_PC + 0, Types.toCARD16(opcode.code, 0));
+		// data
+		Memory.writeReal16(ra_LF + n, 0xCAFE);
+		// execute
+		Interpreter.execute();
+		// check result
+		// pc
+		assertEquals(Processor.savedPC + 1, Memory.PC());
+		// sp
+		assertEquals(1, Processor.SP);
+		// stack contents
+		assertEquals(Memory.readReal16(ra_LF + n), Processor.stack[0]);
+		// memory
+	}
+
+	@Test
+	public void LLB() {
+		logger.info(StackUtil.getCallerMethodName());
+
+		int n = 20;
+		var opcode = Opcode.LLB;
+		
+		// opcode
+		Memory.writeReal16(ra_PC + 0, Types.toCARD16(opcode.code, n));
+		// data
+		Memory.writeReal16(ra_LF + n, 0xCAFE);
+		// execute
+		Interpreter.execute();
+		// check result
+		// pc
+		assertEquals(Processor.savedPC + 2, Memory.PC());
+		// sp
+		assertEquals(1, Processor.SP);
+		// stack contents
+		assertEquals(Memory.readReal16(ra_LF + n), Processor.stack[0]);
+		// memory
+	}
 	
 }
