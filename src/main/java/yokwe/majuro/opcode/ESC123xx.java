c9cf810b0a536ad81569d01a343a6d3f1f76b268
hasegawa <hasegawa@localhost>
2022-02-21 11:50:32 +0900

    Implements more opcode.

--- src/main/java/yokwe/majuro/opcode/ESC123xx.java
+++ src/main/java/yokwe/majuro/opcode/ESC123xx.java
@@ -76,49 +76,49 @@ public class ESC123xx {
 	@Register(Opcode.RRPSB)
 	public static final void OP_RRPSB() {
 		if (Debug.ENABLE_TRACE_OPCODE) logger.debug("TRACE %6o  %-6s", savedPC, Opcode.RRPSB.name);
-		throw new UnexpectedException(); // FIXME
+		push(psbHandle(Processor.PSB));
 	}
 
 	// 171 RRMDS
 	@Register(Opcode.RRMDS)
 	public static final void OP_RRMDS() {
 		if (Debug.ENABLE_TRACE_OPCODE) logger.debug("TRACE %6o  %-6s", savedPC, Opcode.RRMDS.name);
-		throw new UnexpectedException(); // FIXME
+		push(Processor.MDS >>> WORD_BITS);
 	}
 
 	// 172 RRWP
 	@Register(Opcode.RRWP)
 	public static final void OP_RRWP() {
 		if (Debug.ENABLE_TRACE_OPCODE) logger.debug("TRACE %6o  %-6s", savedPC, Opcode.RRWP.name);
-		throw new UnexpectedException(); // FIXME
+		push(Processor.WP);
 	}
 
 	// 173 RRWDC
 	@Register(Opcode.RRWDC)
 	public static final void OP_RRWDC() {
 		if (Debug.ENABLE_TRACE_OPCODE) logger.debug("TRACE %6o  %-6s", savedPC, Opcode.RRWDC.name);
-		throw new UnexpectedException(); // FIXME
+		push(Processor.WDC);
 	}
 
 	// 174 RRPTC
 	@Register(Opcode.RRPTC)
 	public static final void OP_RRPTC() {
 		if (Debug.ENABLE_TRACE_OPCODE) logger.debug("TRACE %6o  %-6s", savedPC, Opcode.RRPTC.name);
-		throw new UnexpectedException(); // FIXME
+		push(Processor.PTC);
 	}
 
 	// 175 RRIT
 	@Register(Opcode.RRIT)
 	public static final void OP_RRIT() {
 		if (Debug.ENABLE_TRACE_OPCODE) logger.debug("TRACE %6o  %-6s", savedPC, Opcode.RRIT.name);
-		throw new UnexpectedException(); // FIXME
+		pushLong(Processor.IT());
 	}
 
 	// 176 RRXTS
 	@Register(Opcode.RRXTS)
 	public static final void OP_RRXTS() {
 		if (Debug.ENABLE_TRACE_OPCODE) logger.debug("TRACE %6o  %-6s", savedPC, Opcode.RRXTS.name);
-		throw new UnexpectedException(); // FIXME
+		push(Processor.XTS);
 	}
 
 	// 211 CALLAGENT
