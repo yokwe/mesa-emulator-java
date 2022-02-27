cb49105b5686dafa6a28f60847c1a2b33e3b444d
hasegawa <hasegawa@localhost>
2022-02-28 00:00:05 +0900

    Add more methods.

--- src/main/java/yokwe/majuro/mesa/Types.java
+++ src/main/java/yokwe/majuro/mesa/Types.java
@@ -40,6 +40,7 @@ public final class Types {
 	public static @Mesa.CARD8 int toCARD8(@Mesa.CARD8 int left, @Mesa.CARD8 int right) {
 		return toCARD8((left << NIBBLE_BITS) | (right & NIBBLE_MASK));
 	}
+	
 	//
 	// toCARD16
 	//
@@ -48,6 +49,69 @@ public final class Types {
 	}
 	public static @Mesa.CARD16 int toCARD16(@Mesa.CARD8 int left, @Mesa.CARD8 int right) {
 		return toCARD16((left << BYTE_BITS) | (right & BYTE_MASK));
+	}
+	
+	// 2.1.3.1 Basic Logical Operators
+	public static int shift(@Mesa.CARD16 int data, int count) {
+		if (0 < count) {
+			if (16 <= count) return 0;
+			return toCARD16(data << count);
+		}
+		if (count < 0) {
+			if (count <= -16) return 0;
+			return toCARD16(data >>> (-count));
+		}
+		return data;
+	}
+	
+	public static int rotate(@Mesa.CARD16 int data, int count) {
+		if (0 < count) {
+			if (16 <= count) count = count % 16;
+			int t = data << count;
+			return toCARD16(t) | toCARD16(t >> 16);
+		}
+		if (count < 0) {
+			if (count <= -16) count = -(-count % 16);
+			int t = data << (16 + count);
+			return toCARD16(t) | toCARD16(t >> 16);
+		}
+		return data;
+	}
+	
+	// 2.1.3.2 Basic Arithmetic Operator
+	public static @Mesa.INT16 int arithShift(@Mesa.INT16 int data, int count) {
+		if (0 < count) {
+			if (16 <= count) return 0;
+			return toCARD16(((data << count) & 0x7fff) | (data & 0x8000));
+		}
+		if (count < 0) {
+			if (count <= -16) return 0;
+			return toCARD16(data >> (-count));
+		}
+		return data;
+	}
+	
+	public static @Mesa.CARD32 int longShift(@Mesa.CARD32 int data, int count) {
+		if (0 < count) {
+			if (32 <= count) return 0;
+			return data << count;
+		}
+		if (count < 0) {
+			if (count <= -32) return 0;
+			return data >>> -count;
+		}
+		return data;
+	}
+	public static @Mesa.INT32 int longArithShift(@Mesa.INT32 int data, int count) {
+		if (0 < count) {
+			if (32 <= count) return 0;
+			return ((data << count) & 0x7fffffff) | (data & 0x80000000);
+		}
+		if (count < 0) {
+			if (count <= -32) return 0;
+			return data >> -count;
+		}
+		return data;
 	}
 
 }
